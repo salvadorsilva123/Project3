04f4dcca0ea6f3f9d249dec5b1ea4100c53abf19
ssilva <ssilva@ecomac.cl>
2024-04-18 12:49:19 -0400

    ruta y controlador

---
+++ resources/views/clients/index.blade.php
@@ -0,0 +1,40 @@
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Document</title>
+    </head>
+    <body>
+        <p>Pagina Clientes</p>
+
+<button>crear</button>
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">id</th>
+      <th scope="col">nombre</th>
+      <th scope="col">apellido</th>
+      <th scope="col">correo</th>
+      <th scope="col">telefono</th>
+      <th scope="col">direccion</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+      <td>Mark</td>
+      <td>Otto</td>
+    </tr>
+    
+  </tbody>
+</table>
+
+
+
+    </body>
+    </html>
